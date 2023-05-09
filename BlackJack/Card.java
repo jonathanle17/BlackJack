75f2714c39c340fa1cfc9b0e263ba7230451e128
Jonathan Le <jonathan.le@mrjaffesclass.com>
2023-05-09 12:47:11 -0700

    12.41

--- BlackJack/Card.java
+++ BlackJack/Card.java
@@ -1,4 +1,4 @@
-
+ 
 /**
  * Write a description of class Card here.
  * An implementation of a card type.
@@ -14,5 +14,70 @@ public class Card
     public Card(Suit aSuit, int aNumber) {
       this.mySuit = aSuit;
       this.myNumber = aNumber; 
+    }
+    
+    public int getNumber() {
+        return myNumber;
+    }
+    
+    public String toString() {
+        String numStr;
+        
+        switch(this.myNumber) {
+            
+        case 2:
+            numStr = "Two";
+            break;
+                
+        case 3:
+            numStr = "Three";
+            break;
+            
+        case 4:
+            numStr = "Four";
+            break;
+        
+        case 5:
+            numStr = "Five";
+            break;
+        
+        case 6:
+            numStr = "Six";
+            break;
+        
+        case 7:
+            numStr = "Seven";
+            break;
+        
+        case 8:
+            numStr = "Eight";
+            break;
+        
+        case 9:
+            numStr = "Nine";
+            break;
+            
+        case 10:
+            numStr = "Ten";
+            break;
+            
+        case 11:
+            numStr = "Jack";
+            break;
+            
+        case 12:
+            numStr = "Queen";
+            break;
+            
+        case 13:
+            numStr = "King";
+            break;
+            
+        case 14:
+            numStr = "Ace";
+            break;
+        }
+        
+        return numStr + " of " + mySuit
     }
 }
