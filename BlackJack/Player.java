9b105cc473d0b58e20e629444402efce262368cf
Jonathan Le <jonathan.le@mrjaffesclass.com>
2023-05-16 12:46:34 -0700

    53.44

--- BlackJack/Player.java
+++ BlackJack/Player.java
@@ -27,6 +27,40 @@ public class Player
     }
     
     public boolean addCard(Card aCard) {
+        //print error if max cards (should never)
+        if (this.numCards == 10) {
+            System.err.printf("%/s's hand already has 10 cards; " +
+                    "cannot add another\n", this.name);
+            System.exit(1);
+        }
         
+        //add new card in next slot and increment number of cards counter
+        this.hand[this.numCards] = aCard;
+        this.numCards++;
+        
+        return (this.getHandSum() <= 21);
+    }
+    
+    public int getHandSum() {
+       int handSum = 0;
+       int cardNum;
+       int numAces = 0;
+       
+       //calculate each card's contribution to hand sum
+       for (int c = 0; c < this.numCards; c++) {
+           cardNum = this.hand[c].getNumber();
+           
+           if (cardNum == 1) {
+               numAces++;
+               handSum += 11;
+           } else if (cardNum > 10) {
+               handSum += 10;
+           } else {
+               handSum += cardNum;
+           }
+       }
+       
+       
+       return handSum;
     }
 }
